932ac20c96b5ec3a6a6958df63cd3c9487001d55
cav-ipb <vcardent@ipb-halle.de>
2025-10-14 10:58:02 -0400

    fix: fix landing assets module not loading

--- aw-dwh/projects/dagster/adventureworks_orchestration/jobs.py
+++ aw-dwh/projects/dagster/adventureworks_orchestration/jobs.py
@@ -1,9 +1,10 @@
 import dagster as dg
-from .constants import ASSET_GROUP_LABS
+from .constants import *
 
 sample_job = dg.define_asset_job(
         "sample_job",
-        selection=dg.AssetSelection.groups(ASSET_GROUP_LABS)
+        selection=dg.AssetSelection.groups(ASSET_GROUP_LABS),
+        executor_def=dg.multiprocess_executor.configured({"max_concurrent": 2}) # This limits the number of concurrent processes
     )
 
 five_minutes_schedule = dg.ScheduleDefinition(
