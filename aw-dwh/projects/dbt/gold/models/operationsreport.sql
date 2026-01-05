0c969d56a8dd6155f4d53d225a4d1ae99f75b176
mariandc18 <ma907222@gmail.com>
2026-01-05 02:14:00 -0500

    modify operationsreport model in gold

--- aw-dwh/projects/dbt/gold/models/operationsreport.sql
+++ aw-dwh/projects/dbt/gold/models/operationsreport.sql
@@ -14,10 +14,6 @@ dimlocation as (
     select *
     from { { ref('dimlocation') } }
 ),
-dimscrapreason as (
-    select *
-    from { { ref('dimscrapreason') } }
-),
 dimdate as (
     select *
     from { { ref('dimdate') } }
@@ -29,13 +25,11 @@ routing_detail as (
         dl.name as location_name,
         dp.name as product_name,
         dd.date as operation_date,
-        fwr.actualresourcehrs,
         fwr.plannedcost,
         fwr.actualcost,
         fwo.scrappedqty,
         fwo.orderqty,
         fwo.is_late,
-        dsr.name as scrap_reason,
         case
             when fwo.scrappedqty > 0 then 1
             else 0
@@ -45,7 +39,6 @@ routing_detail as (
         inner join dimproduct dp on fwr.productid = dp.productid
         inner join dimlocation dl on fwr.locationid = dl.locationid
         left join dimdate dd on fwo.duedateid = dd.dateid
-        left join dimscrapreason dsr on fwo.scrapreasonid = dsr.scrapreasonid
 )
 select operationsequence,
     operation_date,
@@ -62,39 +55,10 @@ select operationsequence,
             when is_late = 1 then workorderid
         end
     ) as late_orders,
-    round(
-        count(
-            distinct case
-                when is_late = 1 then workorderid
-            end
-        ) * 100.0 / nullif(count(distinct workorderid), 0),
-        2
-    ) as late_rate_pct,
     round(sum(plannedcost), 2) as total_planned_cost,
-    round(sum(actualcost), 2) as total_actual_cost,
-    round(sum(actualcost) - sum(plannedcost), 2) as cost_variance,
-    round(
-        (sum(actualcost) - sum(plannedcost)) * 100.0 / nullif(sum(plannedcost), 0),
-        2
-    ) as cost_variance_pct,
-    round(sum(actualresourcehrs), 2) as total_resource_hours,
-    round(avg(actualresourcehrs), 2) as avg_resource_hours,
-    coalesce(
-        max(
-            case
-                when scrap_reason is not null then scrap_reason
-            end
-        ),
-        case
-            when max(scrappedqty) > 0 then 'Rechazo sin causa'
-            else 'Sin rechazo'
-        end
-    ) as primary_scrap_reason
+    round(sum(actualcost), 2) as total_actual_cost
 from routing_detail
 group by operation_date,
     operationsequence,
     location_name,
     product_name
-order by operation_date desc,
-    defect_rate_pct desc,
-    cost_variance desc
