a601ba625bc60c3969f793da1c5f39798ea5ceb7
mariandc18 <ma907222@gmail.com>
2026-01-07 15:01:39 -0500

    delete dimuser in gold

--- aw-dwh/projects/dbt/gold/models/dimuser.sql
+++
@@ -1,8 +0,0 @@
-select {{ dbt_utils.generate_surrogate_key(["id"
-    ]) }} as userid,
-    id as altuserid,
-    firstname,
-    lastname,
-    email,
-    birthdate
-from {{ source('silver', 'silver_users') }}
