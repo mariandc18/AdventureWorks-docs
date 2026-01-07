2cf73bde02b7663520ec2e614e6e043105fa04ce
mariandc18 <ma907222@gmail.com>
2026-01-07 16:02:32 -0500

    delete dimstorereviews model in gold

--- aw-dwh/projects/dbt/gold/models/dimstorereviews.sql
+++
@@ -1,9 +0,0 @@
-select {{ dbt_utils.generate_surrogate_key(
-        [
-        "'reviews'",
-        "id"
-    ]
-    ) }} as storeid,
-    id as altstoreid,
-    name
-from {{ source('silver', 'silver_stores') }}
